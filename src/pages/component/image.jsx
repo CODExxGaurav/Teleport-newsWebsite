017b8ff19cf42647db070ae2cb3c38cb81de1bbe
CODExxGaurav <89847886+CODExxGaurav@users.noreply.github.com>
2023-03-23 20:21:16 +0530

    initial commit

---
+++ src/pages/component/image.jsx
@@ -0,0 +1,3 @@
+const IMAGE  = (prop) => {
+return <img  className = "news-img" src ={prop.src}  width = {300} height = {300} alt={"Image here"} ></img> }
+export default IMAGE ;
---
+++ src/pages/component/navBar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "./menu";
+const Nav = () => {
+  const logo1 = "Daily";
+  const logo2 = "News";
+
+  return (
+   <> <div className="top-nav">
+    <div className="container">
+    <div className="cont-head">
+    <span className = "main-logo"><Image src={"/logo.png"} width={37} height = {35} alt = {"this is an logo image"}></Image></span>
+      <div >
+      <p style={{paddingTop: "11px"}}>{logo1 + logo2}</p>
+        <p className="cont-down">
+        </p>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div className="cont-input">
+      <form>
+        <input placeholder="Search here" name="search"></input>{" "}
+        <button>
+          <span className="span">Search</span>
+        </button>
+      </form>
+    </div>
+    </div>
+</div>
+  </>
+  );
+};
+export default Nav;
